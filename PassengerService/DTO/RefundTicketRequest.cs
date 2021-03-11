c1d4f4b4c3b7564c932bc3637e2ce080ff004f02
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 01:47:03 +0300

    Create RefundTicketRequest.cs
    Add RefundTicketRequest class definition

---
+++ PassengerService/DTO/RefundTicketRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class RefundTicketRequest
+    {
+        public RefundTicketRequest(Guid passengerId, FlightTicket ticket)
+        {
+            PassengerId = passengerId;
+            Ticket = ticket;
+
+            //TIMESTAMP
+        }
+
+        public Guid PassengerId { get; init; }
+
+        public FlightTicket Ticket { get; init; }
+
+        //TIMESTAMP
+    }
+}
