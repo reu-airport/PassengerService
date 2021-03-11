378de49141535f0efc0e1adf87c3d817d65fe9ad
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 01:59:10 +0300

    Create CheckInRequest.cs
    Add CheckInRequest class definition

---
+++ PassengerService/DTO/CheckInRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class CheckInRequest
+    {
+        public CheckInRequest(Guid passengerId, FlightTicket ticket)
+        {
+            PassengerId = passengerId;
+            Ticket = ticket;
+
+            //TIMESTAMP
+        }
+
+        public Guid PassengerId { get; init; }
+
+        public FlightTicket Ticket { get; init; }
+
+        //TIMESTAMP
+    }
+}
