fee69b4c90197988ff90d57cc7f0d2c5072181ec
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 01:56:00 +0300

    Create RefundTicketResponse.cs
    Add RefundTicketResponse class definition

---
+++ PassengerService/DTO/RefundTicketResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class RefundTicketResponse
+    {
+        public RefundTicketResponse(Guid passengerId, FlightTicket ticket, bool isRefunded)
+        {
+            PassengerId = passengerId;
+            Ticket = ticket;
+            IsRefunded = isRefunded;
+        }
+
+        public Guid PassengerId { get; init; }
+
+        //ne ponel nahuya
+        public FlightTicket Ticket { get; init; }
+
+        public bool IsRefunded { get; init; }
+    }
+}
