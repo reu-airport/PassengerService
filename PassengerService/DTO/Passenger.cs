890bb318a20b2266c624d38b4b901009e6e741f9
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 01:32:00 +0300

    Create Passenger.cs
    Add Passenger class & Passenger enum definitions

---
+++ PassengerService/DTO/Passenger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class Passenger
+    {
+        public Passenger()
+        {
+            //TODO
+        }
+
+        public Guid Id { get; init; }
+
+        //might be useless
+        public PassengerStatus Status { get; set; }
+
+        public FlightTicket Ticket { get; set; } = null;
+    }
+
+    //might be useless
+    public enum PassengerStatus
+    {
+
+    }
+}
