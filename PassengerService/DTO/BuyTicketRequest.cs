24f3ed01e4dda6ed9af1269e6e8920b82dc735b4
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 01:38:58 +0300

    Create BuyTicketRequest.cs
    Add BuyTicketRequest class definition

---
+++ PassengerService/DTO/BuyTicketRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class BuyTicketRequest
+    {
+        public BuyTicketRequest(Guid passengerId, Guid flightId, bool hasBaggage, bool isVip)
+        {
+            PassengerId = passengerId;
+            FlightId = flightId;
+            HasBaggae = hasBaggage;
+            IsVip = isVip;
+
+            //TIMESTAMP
+        }
+
+        public Guid PassengerId { get; init; }
+
+        public Guid FlightId { get; init; }
+
+        public bool HasBaggae { get; init; }
+
+        public bool IsVip { get; init; }
+
+        //TIMESTAMP
+    }
+}
