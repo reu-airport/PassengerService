01ee1871bee92360518b9196f9be783f06acc7b6
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 01:43:22 +0300

    Create BuyTicketResponse.cs
    Add BuyTicketResponse class  & BuyTicketResponseStatus enum definitions

---
+++ PassengerService/DTO/BuyTicketResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class BuyTicketResponse
+    {
+        public BuyTicketResponse(Guid passengerId, BuyTicketResponseStatus status, FlightTicket ticket)
+        {
+            PassengerId = passengerId;
+            Status = status;
+            Ticket = ticket;
+        }
+
+        public Guid PassengerId { get; init; }
+
+        //might be useless
+        public BuyTicketResponseStatus Status { get; init; }
+
+        public FlightTicket Ticket { get; init; }
+    }
+
+    //might be useless
+    public enum BuyTicketResponseStatus
+    {
+        //TODO
+    }
+}
