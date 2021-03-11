39683e7b6cdfa34a2204653723ec4daaef76ce9d
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-03-12 02:05:49 +0300

    Create CheckInResponse.cs
    Add CheckInResponse, BoardingPass class & CheckInResponseStatus enum definitions

---
+++ PassengerService/DTO/CheckInResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PassengerService.DTO
+{
+    public class CheckInResponse
+    {
+        public CheckInResponse(Guid passengerId, BoardingPass pass, CheckInResponseStatus status)
+        {
+            PassengerId = passengerId;
+            Pass = pass;
+            Status = status;
+        }
+
+        public Guid PassengerId { get; init; }
+
+        //might be useless
+        public BoardingPass Pass { get; init; }
+
+        //might be useless
+        public CheckInResponseStatus Status { get; init; }
+    }
+
+    //might be useless
+    public enum CheckInResponseStatus
+    {
+        //TODO
+    }
+
+    //might be useless
+    public class BoardingPass
+    {
+        //TODO
+    }
+}
