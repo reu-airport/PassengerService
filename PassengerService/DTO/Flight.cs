f510c059038d4d88e0b3932b9137ac4b788349e9
Dionissiy <32749678+Dionissiy@users.noreply.github.com>
2021-04-07 21:22:29 +0300

    Remove useless DTO and add flight dto class

---
+++ PassengerService/DTO/Flight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PassengerService.DTO
+{
+    public class Flight
+    {
+        public Flight()
+        {
+        }
+
+        Guid Id { get; init; }
+
+        FlightDirection Direction{ get; init; }
+
+        //time {get; init; }
+
+        
+
+    }
+
+    public enum FlightDirection
+    {
+        Depurture,
+        Arrrival
+    }
+
+    
+}
